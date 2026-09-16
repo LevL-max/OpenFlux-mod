842ff576b958a0b03ceb7ebce74293c94c4ca2ac
LevL-max <levlvov@hotmail.com>
2026-09-16 16:01:32 +0100

    Add V6 atomic max helper for carrier telemetry

---
+++ transport/yandex/volga_v6_atomic.go
@@ -0,0 +1,15 @@
+package yandex
+
+import "sync/atomic"
+
+func atomicMax(dst *atomic.Uint64, value uint64) {
+	for {
+		current := dst.Load()
+		if value <= current {
+			return
+		}
+		if dst.CompareAndSwap(current, value) {
+			return
+		}
+	}
+}
